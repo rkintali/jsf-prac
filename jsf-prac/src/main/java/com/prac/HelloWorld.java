7733a985caf821f04a6d480216539ac5c591015b
RK <rajsekhar.kintali@gmail.com>
2016-05-14 12:54:11 +1000

    change config to run on Jetty

--- jsf-prac/src/main/java/com/prac/HelloWorld.java
+++ jsf-prac/src/main/java/com/prac/HelloWorld.java
@@ -2,7 +2,6 @@ package com.prac;
 
 import java.io.Serializable;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -22,28 +21,23 @@ public class HelloWorld implements Serializable{
 		System.out.println("HelloWorld started!");
 	}
 
-	@PostConstruct
+	
 	public void init() {
 		System.out.println("Post construct method - executed");
 		this.setMessage("This is a message from post construct");
 	}
 
 	public String getMessage() {
+		message = "test";
 		System.out.println("Getting message");
-		message = "This is a smiple message";
-		/*if (messageBean != null) {
-			message = messageBean.getMessage();
-		} else {
-			message = "This is a simple message";
-		}*/
+		
 		return message;
 	}
-
+	
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
-	/*public void setMessageBean(Message message) {
-		this.messageBean = message;
-	}*/
+	
+
 }
